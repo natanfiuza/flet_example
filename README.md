0b9aa9e7fd657eb600c1ed5ed253020a89f484df
NatanFiuza <natan.fiuza@gmail.com>
2024-02-24 17:16:41 -0300

    Alterando o README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Exemplo de aplicação em Flet
+
+[Flet](https://flet.dev/)
+
+[Documentação do Flet](https://flet.dev/docs/)
+
+
+## Instalar 
+
+Para utilizar o teste instale o gerenciador de ambientes [Pipenv](https://pipenv.pypa.io/en/latest/) veja o passo a passo abaixo:
+
+1. Instalar o pipenv
+
+```bash
+
+pip install pipenv
+
+```
+
+2. Instalar as dependencias
+
+```bash
+
+pipenv install
+
+```
+
+3. Iniciar o ambiente virtual
+
+```bash
+
+pipenv shell
+
+```
--- app.py
+++ app.py
@@ -13,16 +13,37 @@ session = Session()
 
 def main(page: ft.Page):
     page.title = "Cadastro Produto Exemplo"
-    page.bgcolor = 'white'
+    page.bgcolor = ''
     lista_produtos = ft.ListView()
   
     def cadastrar(e):
-      novo_produto = Produto(titulo=produto.value, preco=preco.value)
-      session.add(novo_produto)
-      session.commit()
-      print('Produto salvo!!')
-      produto.value = ''
+        try: 
+            novo_produto = Produto(titulo=produto.value, preco=preco.value)
+            session.add(novo_produto)
+            session.commit()
+            print('Produto salvo!!')
+            lista_produtos.controls.append(
+                ft.Container(
+                    ft.Text(produto.value),
+                    bgcolor=ft.colors.BLACK12,
+                    padding=15,
+                    alignment=ft.alignment.center,
+                    margin=3,
+                    border_radius=10,
+                )
+            )
+            produto.value = ''
+            preco.value = ''
+            txt_error.visible = False
+            txt_acerto.visible = True
+        except:    
+            txt_error.visible = True
+            txt_acerto.visible = False
+        page.update()
 
+
+    txt_error = ft.Container(ft.Text('Erro ao cadastrar o produto!!'), visible=False, bgcolor=ft.colors.RED,padding=10, alignment=ft.alignment.center)
+    txt_acerto = ft.Container(ft.Text('Produto salvo com sucesso!!'), visible=False, bgcolor=ft.colors.GREEN,padding=10, alignment=ft.alignment.center)
     txt_titulo = ft.Text('Cadastro do Produto')    
     page.add(txt_titulo)
     
@@ -30,9 +51,24 @@ def main(page: ft.Page):
     preco = ft.TextField(label="Preço",text_align=ft.TextAlign.LEFT)
 
     btn_cadastrar = ft.ElevatedButton('Cadastrar', on_click=cadastrar)
-    page.add(produto,preco,btn_cadastrar)
+    page.add(
+        txt_error,
+        txt_acerto,
+        produto,
+        preco,
+        btn_cadastrar
+    )
     for p in session.query(Produto).all():
-        lista_produtos.controls.append(ft.Text(p.titulo))
+        lista_produtos.controls.append(
+            ft.Container(
+                ft.Text(p.titulo),
+                bgcolor=ft.colors.BLACK12,
+                padding=15,
+                alignment=ft.alignment.center,
+                margin=3,
+                border_radius=10,
+            )
+        )
     page.add(
         lista_produtos,
     )
